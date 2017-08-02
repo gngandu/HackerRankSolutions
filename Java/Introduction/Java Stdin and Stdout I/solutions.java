22ee8210863af64b604f03bb76f8aded61111689
George Kamau Ngandu <gngandu@users.noreply.github.com>
2017-08-02 22:26:42 +0300

    Create solutions.java

---
+++ Java/Introduction/Java Stdin and Stdout I/solutions.java
@@ -0,0 +1,22 @@
+/* 
+	 @author George K. Ngandu 
+     Github: github.com/gngandu
+	 HackerRank: hackerrank.com/gngandu
+
+*/
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+      System.out.println(b);
+        System.out.println(c);
+    }
+}
