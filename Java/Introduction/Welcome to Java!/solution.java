55777a8373bf85fc7305232ea8bebc3e0aae6998
gngandu <georgekamautm@gmail.com>
2017-08-02 23:25:25 +0300

    Hacker Rank Solutions

---
+++ Java/Introduction/Welcome to Java!/solution.java
@@ -0,0 +1,13 @@
+/*	Author: George Ngandu
+    Github: github.com/gngandu
+	HackerRank: hackerrank.com/gngandu
+ */
+ 
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+        
+    }
+}
